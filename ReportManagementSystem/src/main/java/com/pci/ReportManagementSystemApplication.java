24819111c504eeb94896e2a7c84f04a8cdf69d05
s_honda_k01 <s_honda_k01@pci-sol.com>
2020-07-30 16:00:15 +0900

    first commit

---
+++ ReportManagementSystem/src/main/java/com/pci/ReportManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportManagementSystemApplication.class, args);
+	}
+
+}
